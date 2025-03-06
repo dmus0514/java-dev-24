5d3ebef5b14f0d771df900b5e4985f2bf833721f
dmus0514 <dmitry.uskov@netcracker.com>
2025-03-06 23:48:04 +0300

    hw15-patterns

--- hw15-structuralPatterns/src/main/java/ru/otus/HomeWork.java
+++ hw15-structuralPatterns/src/main/java/ru/otus/HomeWork.java
@@ -1,6 +1,20 @@
 package ru.otus;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.handler.ComplexProcessor;
+import ru.otus.listener.ListenerPrinterConsole;
+import ru.otus.model.Message;
+import ru.otus.model.ObjectForMessage;
+import ru.otus.processor.homework.ProcessorEvenSecondException;
+import ru.otus.processor.homework.ProcessorSwapFields;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class HomeWork {
+
+    private static final Logger logger = LoggerFactory.getLogger(HomeWork.class);
 
     /*
     Реализовать to do:
@@ -16,9 +30,27 @@ public class HomeWork {
     */
 
     public static void main(String[] args) {
-        /*
-          по аналогии с Demo.class
-          из элеменов "to do" создать new ComplexProcessor и обработать сообщение
-        */
+        var processors = List.of(
+                new ProcessorSwapFields(),
+                new ProcessorEvenSecondException(LocalDateTime::now)
+        );
+
+        var complexProcessor = new ComplexProcessor(processors, ex -> {});
+        var listenerPrinter = new ListenerPrinterConsole();
+        complexProcessor.addListener(listenerPrinter);
+
+        var field13 = new ObjectForMessage();
+        field13.setData(List.of("field13_1", "field13_2", "field13_3"));
+
+        var message = new Message.Builder(1L)
+                .field11("field11")
+                .field12("field12")
+                .field13(field13)
+                .build();
+
+        var result = complexProcessor.handle(message);
+        logger.info("result:{}", result);
+
+        complexProcessor.removeListener(listenerPrinter);
     }
 }
