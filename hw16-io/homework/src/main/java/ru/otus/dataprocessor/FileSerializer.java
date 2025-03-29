111b01927efffd50f327860d39ed99592295c4b9
dmus0514 <dmitry.uskov@netcracker.com>
2025-03-29 23:09:22 +0300

    hw16-io

---
+++ hw16-io/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -0,0 +1,25 @@
+package ru.otus.dataprocessor;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+public class FileSerializer implements Serializer {
+
+    private final String fileName;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    public FileSerializer(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void serialize(Map<String, Double> data) {
+        try {
+            objectMapper.writeValue(new File(fileName), data);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
